828af8be66948d462eea26008f548cbd583db1ff
Jeff Harwell <jharwell@fuller.edu>
2013-03-25 00:51:45 -0700

    Part 7 Done, iscroll is working

--- js/HomeView.js
+++ js/HomeView.js
@@ -8,6 +8,13 @@ var HomeView = function(store) {
     this.findByName = function() {
         store.findByName($('.search-key').val(), function(employees) {
             $('.employee-list').html(HomeView.liTemplate(employees));
+            if (self.iscroll) {
+                console.log('Refresh iScroll');
+                self.iscroll.refresh();
+            } else {
+                console.log('New iScroll');
+                self.iscroll = new iScroll($('.scroll', self.el)[0], {hScrollbar: false, vScrollbar: false });
+            }
         });
     };
 
@@ -22,4 +29,4 @@ var HomeView = function(store) {
 }
 
 HomeView.template = Handlebars.compile($("#home-tpl").html());
-HomeView.liTemplate = Handlebars.compile($("employee-li-tpl").html());
+HomeView.liTemplate = Handlebars.compile($("#employee-li-tpl").html());
