7ae012409e90944db9f7f2479b03d5fc94a5dde5
Jeff Harwell <jharwell@fuller.edu>
2013-03-25 14:21:15 -0700

    Working on part 9, it is not working in emulate.phonegap. I want to test on my phone to see if it is an emulator error of a code error

--- js/main.js
+++ js/main.js
@@ -8,8 +8,31 @@ var app = {
         }
     },    
 
+    route: function() {
+        console.log('app.route() Called');
+        var hash = window.location.hash;
+        console.log('Hash is '+hash);
+        if (!hash) {
+            $('body').html(new HomeView(this.store).render().el);
+            return;
+        }
+
+        var match = hash.match(app.detailsURL);
+
+        if (match) {
+            console.log('Got a match');
+            this.store.findById(Number(match[1]), function(employee) {
+                $('body').html(new EmployeeView(employee).render().el)
+            });
+        }
+    },
+
     registerEvents: function() {
         var self = this;
+        // Event listener that listens to the URL has tag
+        $(window).on('hashchanges', $.proxy(this.route, this));
+
+
         // Check if the browser supports touch events
         if (document.documentElement.hasOwnProperty('ontouchstart')) {
             // yes, so register our touch event listener
@@ -32,13 +55,17 @@ var app = {
 
     initialize: function() {
         var self = this;
+        
+        // regular expression that matches employee details urls
+        this.detailsURL = /^#employees\/(\d{1,})/;
+
         // Register our event listeners
         self.registerEvents();
 
         // Initialize our Memore Store and render the home page
         this.store = new MemoryStore(function() {
             self.showAlert('Store Initialized', 'Info');
-            $('body').html(new HomeView(self.store).render().el);
+            self.route();
         });
     }
 
